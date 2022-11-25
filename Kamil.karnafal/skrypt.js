5b8822b461042183ffb972c93b48a33033d29d87
JaworsMum <97956077+JaworsMum@users.noreply.github.com>
2022-11-25 09:15:36 +0100

    Add files via upload

---
+++ Kamil.karnafal/skrypt.js
@@ -0,0 +1,12 @@
+function Oblicz(){
+    var a1=(document.getElementById("a1").value);
+    var an=(document.getElementById("an").value);
+    var n=(document.getElementById("n").value);
+    var wynik=(an-a1)/(n-1)
+    document.getElementById("wynik").value=wynik;
+
+   
+    
+       
+    }
+ 
